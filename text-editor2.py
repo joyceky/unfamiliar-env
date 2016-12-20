1093b1de8e7be2bff494a7dd7a2ee2a23fdc02dc
Kyle Joyce <kyyjoyce@gmail.com>
2016-12-19 18:07:51 -0700

    editor working and saving

--- text-editor2.py
+++ text-editor2.py
@@ -6,42 +6,37 @@ import tkinter.filedialog
 import tkinter.messagebox
 
 # Create a basic window with text-editing capabilities
-root = tkinter.Tk(className="notes")
-textPad = tkinter.scrolledtext.ScrolledText(root, width=100, height=80)
+root = tkinter.Tk(className="note")
+editor = tkinter.scrolledtext.ScrolledText(root, width=100, height=80)
 
 # defining menu commands
-
 def open_command():
     file = tkinter.filedialog.askopenfile(parent=root,mode='rb',title="Select a file")
     if file != None:
         contents = file.read()
-        textPad.insert('1.0',contents)
+        editor.insert('1.0',contents)
         file.close()
 
-def save_command(self):
+def save_command():
     file = tkinter.filedialog.asksaveasfile(mode="w")
     if file != None:
-        #Remove last character bc extra return is added
-        data = self.textPad.get('1.0, END+'-lc)
-        file.write(data)
+        usertext = str(editor.get(1.0, END))
+        file.write(usertext)
         file.close()
 
 def exit_command():
-    if tkinter.messagebox.askokcancel("Quit", "Confirm exit notes?"):
+    if tkinter.messagebox.askokcancel("Quit", "Press ok to leave note"):
         root.destroy()
 
 def about_command():
-    label = tkinter.messagebox.showinfo("About", "A simple note-taking app written in Python")
+    label = tkinter.messagebox.showinfo("About", "A simple note-taking app written with Python and tkinter")
 
 # creates the basic menu
-def dummy():
-    print("I am a Dummy Command, I will be removed in the next step")
 menu = tkinter.Menu(root)
 root.config(menu=menu)
 
 filemenu = tkinter.Menu(menu)
 menu.add_cascade(label="File", menu=filemenu)
-filemenu.add_command(label="New", command=dummy)
 filemenu.add_command(label="Open...", command=open_command)
 filemenu.add_command(label="Save", command=save_command)
 filemenu.add_separator()
@@ -52,5 +47,5 @@ menu.add_cascade(label="Help", menu=helpmenu)
 helpmenu.add_command(label="About...", command=about_command)
 # end of the menu
 
-textPad.pack()
+editor.pack()
 root.mainloop()
